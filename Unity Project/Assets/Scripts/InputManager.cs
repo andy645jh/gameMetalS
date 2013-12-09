3e805e53a1b65c52fde442827c78e301c74be4bd
nuribe <nuribe@contractor.amplifiy.com>
2013-12-09 14:26:08 -0500

    Ignorando carpeta de libreria y subiendo clase de inputs

--- Unity Project/Assets/Scripts/InputManager.cs
+++ Unity Project/Assets/Scripts/InputManager.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InputManager : MonoBehaviour {
+public class InputManager {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public InputManager()
+	{
+
 	}
 }
--- Unity Project/Assets/Scripts/Main.cs
+++ Unity Project/Assets/Scripts/Main.cs
@@ -3,13 +3,74 @@ using System.Collections;
 
 public class Main : MonoBehaviour {
 
+	private bool _pressLeft = false;
+	private bool _pressRight = false;
+	private bool _pressUp = false;
+	private bool _pressDown = false;
+
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+
+	}	
+
 	void Update () {
-	
+
+		// keys press
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			_pressLeft = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			_pressRight = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			_pressUp = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			_pressDown = true;
+		}
+
+		// keys releases
+		if(Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			_pressLeft = false;
+		}
+		
+		if(Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			_pressRight = false;
+		}
+		
+		if(Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			_pressUp = false;
+		}
+		
+		if(Input.GetKeyUp(KeyCode.DownArrow))
+		{
+			_pressDown = false;
+		}
+
+		// moving right or left
+		if(_pressRight)
+		{
+			gameObject.transform.Translate(new Vector2(Time.deltaTime*2,0));
+		}
+
+		if(_pressLeft)
+		{
+			gameObject.transform.Translate(new Vector2(Time.deltaTime*-2,0));
+		}
+
+		//
 	}
+
+
 }
